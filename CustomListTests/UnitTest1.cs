d27584f7304a0a250ae38bb48905a91d0a9d53ab
telvinmathews <57323861+telvinmathews@users.noreply.github.com>
2020-04-27 14:21:37 -0500

    initial

---
+++ CustomListTests/UnitTest1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Sandbox;
+
+namespace CustomListTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Add_AddingOneValueToEmptyCustomList_AddedValueGoesToIndexZero()
+        {
+            // arrange
+            CustomList<int> testList = new CustomList<int>();
+            int itemToAdd = 10;
+            int expected = 10;
+            int actual;
+
+            // act
+            testList.Add(itemToAdd);
+            actual = testList[0];
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Add_AddingOneValueToEmptyCustomList_CountOfCustomListIncrements()
+        {
+            // arrange
+            CustomList<int> testList = new CustomList<int>();
+            int itemToAdd = 10;
+            int expected = 1;
+            int actual;
+
+            // act
+            testList.Add(itemToAdd);
+            actual = testList.Count;
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+        
+        // what happens if you add multiple things (or add to a CustomList that already has some values)?
+            // what happens to the last-added item?
+            // what happens to the Count?
+
+        // what happens if you add more items than the initial Capacity of the CustomList?
+
+    }
+}
